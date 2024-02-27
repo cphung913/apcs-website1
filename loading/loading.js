3c9b590a771cc8488b85aadec0749377ea9060ad
Chase Phung <69651488+cphung913@users.noreply.github.com>
2024-02-27 17:54:09 +0000

    Tue, Feb 27, 2024, 9:54 AM -08:00

--- loading/loading.js
+++ loading/loading.js
@@ -2,5 +2,7 @@ const background = document.querySelector('.bg');
 const text = document.querySelector('.loading-text');
 
 let load = 0;
+let opacity = 1;
 
 text.innerText = `${load}%`;
+text.style.opacity = opacity;
