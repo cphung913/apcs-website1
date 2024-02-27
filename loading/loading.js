967ebcea5d1e3ff8d526265052e48ddda550852f
Chase Phung <69651488+cphung913@users.noreply.github.com>
2024-02-27 17:57:21 +0000

    Tue, Feb 27, 2024, 9:57 AM -08:00

--- loading/loading.js
+++ loading/loading.js
@@ -3,6 +3,12 @@ const text = document.querySelector('.loading-text');
 
 let load = 0;
 let opacity = 1;
+let blur = 0;
 
-text.innerText = `${load}%`;
-text.style.opacity = opacity;
+function blurring() {
+    load++;
+
+    text.innerText = `${load}%`;
+    text.style.opacity = opacity;
+    background.style.filter = `blur(${blur}px)`;
+}
