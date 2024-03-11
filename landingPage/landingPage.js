6440e5dad268f1757ab3631acd484cf98ab4dfcf
Chase Phung <69651488+cphung913@users.noreply.github.com>
2024-03-11 16:57:08 +0000

    Mon, Mar 11, 2024, 9:57 AM -07:00

--- landingPage/landingPage.js
+++ landingPage/landingPage.js
@@ -1,6 +1,11 @@
 const toggle = document.getElementById('toggle');
-const modal = document.
+const open = document.getElementById('open');
+const modal = document.getElementById('modal')
 
 toggle.addEventListener('click', () => {
     document.body.classList.toggle('show-nav');
-})
+});
+
+open.addEventListener('click', () => {
+    modal.classList.add('show-modal');
+});
